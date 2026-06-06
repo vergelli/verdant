037e1984b78519d1d56594dd566075d68fa55deb
vergelli <federicocusot@gmail.com>
2026-06-06 19:17:30 -0300

    Verdant 2.1.1: remove stray .gitkeep files, bump version

--- core/constants.lua
+++ core/constants.lua
@@ -3,7 +3,7 @@ local Verdant = Verdant
 
 Verdant.Constants = {
   ADDON_NAME    = "Verdant",
-  VERSION       = "2.1.0",
+  VERSION       = "2.1.1",
   SLASH_COMMAND = "/verdant",
 
   DEBUG         = false,
